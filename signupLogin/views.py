34179d5f32f2193868b8aff93c066b9ecad88e78
harshit.bhalla.002 <harshit.bhalla.002@gmail.com>
2024-03-18 23:00:22 +0000

    add ses library to send Email using amazon ses.

--- signupLogin/views.py
+++ signupLogin/views.py
@@ -9,24 +9,29 @@ from django.contrib.auth.forms import AuthenticationForm
 def home(request):
     return render(request, 'home-page.html', {})
 
+#  decides which page to render (home / landing) based on whether the user is authenticated or not
 def entry_view(request):
     if request.user.is_authenticated:
         return render(request, 'home-page.html', {})    
     else:
         return render(request, 'landing-page.html', {})
 
+# athentication view for registration of new users
 def authenticationView(request):
     form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(request.POST or None )
         if form.is_valid():
-            user = form.save(commit=False)
+            email = request.POST.get('email')
+            user = form.save(commit=False) 
+            user.email = email 
             if 'adminUserCheckbox' in request.POST:
                 user.is_staff = True
             user.save()
             return redirect('/login')
     return render(request, 'registration/register.html',{'form' :form})
 
+# login view for existing users
 def login_view(request):
     form = AuthenticationForm()
     if request.method == 'POST':
@@ -43,6 +48,7 @@ def login_view(request):
                 return render(request, 'login.html', {'form': form, 'error_message': error_message})
     return render(request, 'registration/login.html', {'form': form})
 
+# logout view for existing users
 def logout_view(request):
     logout(request)
     return redirect('home') 
