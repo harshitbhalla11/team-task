987bb7139a38dab2fe9299149308aeeb2c812664
Harshit Bhalla <harshit.bhalla.002@gmail.com>
2024-03-19 14:51:27 +0530

    merged

--- signupLogin/views.py
+++ signupLogin/views.py
@@ -4,29 +4,37 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
+from ses_mail.send_ses import SESEmailService
 
 @login_required
 def home(request):
     return render(request, 'home-page.html', {})
 
+#  decides which page to render (home / landing) based on whether the user is authenticated or not
 def entry_view(request):
     if request.user.is_authenticated:
         return render(request, 'home-page.html', {})    
     else:
         return render(request, 'landing-page.html', {})
 
+# athentication view for registration of new users
 def authenticationView(request):
     form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(request.POST or None )
         if form.is_valid():
-            user = form.save(commit=False)
+            email = request.POST.get('email')
+            #send verification email to user from amazon SES
+            SESEmailService.Identify_user_email(email)
+            user = form.save(commit=False) 
+            user.email = email 
             if 'adminUserCheckbox' in request.POST:
                 user.is_staff = True
             user.save()
             return redirect('/login')
     return render(request, 'registration/register.html',{'form' :form})
 
+# login view for existing users
 def login_view(request):
     form = AuthenticationForm()
     if request.method == 'POST':
@@ -43,6 +51,7 @@ def login_view(request):
                 return render(request, 'login.html', {'form': form, 'error_message': error_message})
     return render(request, 'registration/login.html', {'form': form})
 
+# logout view for existing users
 def logout_view(request):
     logout(request)
     return redirect('home') 
